224b214bf23c8ac070d1c3421e860ab342aeed90
seba-pitra <sebastian.pitra10@gmail.com>
2025-01-31 11:34:45 -0300

    config: add connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,41 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { envConfiguration } from './config/env.config';
+
 import { AuthModule } from './modules/auth/auth.module';
+import { joiValidationSchema } from './config/joi.validation';
 import { ReservationsModule } from './modules/reservations/reservations.module';
+import { Reservation } from './modules/reservations/entities/reservation.entity';
+import { User } from './modules/auth/entities/auth.entity';
 
 @Module({
-    imports: [AuthModule, ReservationsModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [envConfiguration],
+      validationSchema: joiValidationSchema,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          entities: [User, Reservation],
+          synchronize: true,
+        };
+      },
+    }),
+
+    AuthModule,
+
+    ReservationsModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envConfiguration } from './config/env.config';
 
 async function bootstrap() {
+  const logger = new Logger('MAIN');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { port } = envConfiguration();
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(port);
+  logger.log(`App running on port ${port}`);
 }
 bootstrap();
